6329c57b4beae9aaaa37b4503849e19e4d6fd4c7
Joaco2025 <joaquinalf177@gmail.com>
2025-12-10 00:23:39 -0700

    Initial commit: Project structure setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-gray-600">
+        Testing Tailwind correct implementation, Hello, Mockmate AI!!!
+      </h1>
+    </div>
+  )
+}
